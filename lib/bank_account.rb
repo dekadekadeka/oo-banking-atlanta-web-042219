27ea97bb2222a7c9035765c0ef59836746bf59c6
github username <dekadekadeka@gmail.com>
2019-04-25 17:14:29 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(money)
+    @balance = self.balance + money
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        # binding.pry
+        @balance > 0 && @status == "open" ? true : false
+    end
+
+    def close_account
+        @status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Transfer
+  #attr_reader :sender, :receiver
+  attr_accessor :status, :amount, :sender, :receiver
   # your code here
+  def initialize(sender, receiver, amount = 50, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    end
+  end
+
+  def execute_transaction
+    
+    receiver.deposit(amount)
+    sender.balance -= amount
+    self.status = "complete"
+    define_singleton_method(:execute_transaction) {}
+    if sender.valid? == false
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
+  end
 end
